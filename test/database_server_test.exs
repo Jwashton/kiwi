a92f6c318428c27e797d608fd6f5f96ca91c2c57
William Ashton <William@AshtonFam.org>
2020-08-17 12:59:24 -0400

    End of part 5.3.2

---
+++ test/database_server_test.exs
@@ -0,0 +1,13 @@
+defmodule DatabaseServerTest do
+  use ExUnit.Case, async: true
+  
+  test "Running a query" do
+    server_pid = DatabaseServer.start()
+    query = "query 1"
+
+    DatabaseServer.run_async(server_pid, query)
+    result = DatabaseServer.get_result()
+    
+    assert Regex.match?(~r{Connection \d+: #{query} result}, result)
+  end
+end
